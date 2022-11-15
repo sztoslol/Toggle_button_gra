a1ed319b4faa36175be4399433316051b539819c
sztoslol <kubuszeski05@wp.pl>
2022-11-15 09:40:03 +0100

    Dodano testowy mechanizm, ktory nawet dziala

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,6 +2,7 @@
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import android.widget.ToggleButton
 
  class MainActivity : AppCompatActivity() {
@@ -13,5 +14,21 @@ import android.widget.ToggleButton
             findViewById(R.id.toggleButton_5),findViewById(R.id.toggleButton_6),findViewById(R.id.toggleButton_7),findViewById(R.id.toggleButton_8),findViewById(R.id.toggleButton_9))
 
         var kolejnosc = arrayListOf<Int>(3,5,1,2,6,4,9,7,8)
+
+        //Toast.makeText(this, buttons[0].id.toString().contains("1").toString(), Toast.LENGTH_SHORT).show()
+
+        var nrKlik = 0
+        var nrButton = 0
+
+        for (i in buttons.indices)
+        {
+            buttons[i].setOnClickListener {
+                nrButton = buttons[i].text.toString().toInt()
+                if (kolejnosc[nrKlik] == nrButton)
+                    nrKlik++
+                else
+                    Toast.makeText(this, "Błąd!", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
